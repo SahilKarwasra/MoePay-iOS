b0aae9f5ccf430c96bda4ca2d0acde25e37b2e95
SahilKarwasra <sahilkarwasra11@gmail.com>
2026-07-06 11:29:25 +0530

    Initial Commit

---
+++ MoePay/MoePayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoePayApp.swift
+//  MoePay
+//
+//  Created by Sahil Karwasrsa on 06/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MoePayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
